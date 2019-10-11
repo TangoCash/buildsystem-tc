16838d029dd943675b815fded3ae44c2159538ca
max_10 <max_10@gmx.de>
2019-10-12 00:39:57 +0200

    - neutrino-plugins.mk: rename NP_OBJ_DIR => NEUTRINO_PLUGINS_OBJ_DIR

--- package/neutrino-plugins/neutrino-plugins.mk
+++ package/neutrino-plugins/neutrino-plugins.mk
@@ -1,16 +1,11 @@
-NP_OBJ_DIR   = $(BUILD_DIR)/neutrino-plugins
-NP_DIR       = $(TARGET_DIR)/usr/share/tuxbox/neutrino/plugins/
-NP_LOGOS_DIR = $(TARGET_DIR)/usr/share/tuxbox/neutrino/icons/logo
-
-# -----------------------------------------------------------------------------
-
 #
 # neutrino-plugins
 #
-NEUTRINO_PLUGINS_VER    = git
-NEUTRINO_PLUGINS_DIR    = neutrino-plugins.$(NEUTRINO_PLUGINS_VER)
-NEUTRINO_PLUGINS_SOURCE = $(NEUTRINO_PLUGINS_DIR)
-NEUTRINO_PLUGINS_URL    = $(MAX-GIT-GITHUB)/neutrino-plugins.git
+NEUTRINO_PLUGINS_VER     = git
+NEUTRINO_PLUGINS_DIR     = neutrino-plugins.$(NEUTRINO_PLUGINS_VER)
+NEUTRINO_PLUGINS_SOURCE  = $(NEUTRINO_PLUGINS_DIR)
+NEUTRINO_PLUGINS_URL     = $(MAX-GIT-GITHUB)/neutrino-plugins.git
+NEUTRINO_PLUGINS_OBJ_DIR = $(BUILD_DIR)/neutrino-plugins
 
 NP_CONFIGURE_ADDITIONS = \
 	$(LOCAL_N_PLUGIN_BUILD_OPTIONS)
@@ -33,9 +28,9 @@ $(D)/neutrino-plugins.do_prepare: | bootstrap ffmpeg libcurl libpng libjpeg-turb
 	@touch $@
 
 $(D)/neutrino-plugins.config.status:
-	rm -rf $(NP_OBJ_DIR)
-	test -d $(NP_OBJ_DIR) || mkdir -p $(NP_OBJ_DIR)
-	cd $(NP_OBJ_DIR); \
+	rm -rf $(NEUTRINO_PLUGINS_OBJ_DIR)
+	test -d $(NEUTRINO_PLUGINS_OBJ_DIR) || mkdir -p $(NEUTRINO_PLUGINS_OBJ_DIR)
+	cd $(NEUTRINO_PLUGINS_OBJ_DIR); \
 		$(SOURCE_DIR)/$(NEUTRINO_PLUGINS_DIR)/autogen.sh $(SILENT_OPT) && automake --add-missing $(SILENT_OPT); \
 		$(BUILD_ENV) \
 		$(SOURCE_DIR)/$(NEUTRINO_PLUGINS_DIR)/configure $(SILENT_OPT) \
@@ -57,12 +52,12 @@ $(D)/neutrino-plugins.config.status:
 	@touch $@
 
 $(D)/neutrino-plugins.do_compile: neutrino-plugins.config.status
-	$(MAKE) -C $(NP_OBJ_DIR) DESTDIR=$(TARGET_DIR)
+	$(MAKE) -C $(NEUTRINO_PLUGINS_OBJ_DIR) DESTDIR=$(TARGET_DIR)
 	@touch $@
 
 $(D)/neutrino-plugins: neutrino-plugins.do_prepare neutrino-plugins.do_compile
 	mkdir -p $(TARGET_DIR)/usr/share/tuxbox/neutrino/icons
-	$(MAKE) -C $(NP_OBJ_DIR) install DESTDIR=$(TARGET_DIR)
+	$(MAKE) -C $(NEUTRINO_PLUGINS_OBJ_DIR) install DESTDIR=$(TARGET_DIR)
 	rm -rf $(addprefix $(TARGET_DIR)/etc/init.d/,K01* S99*)
 	$(TOUCH)
 
@@ -70,10 +65,10 @@ neutrino-plugins-clean:
 	rm -f $(D)/neutrino-plugins
 	rm -f $(D)/neutrino-plugins.config.status
 	rm -f $(D)/neutrino.config.status
-	cd $(NP_OBJ_DIR); \
-		$(MAKE) -C $(NP_OBJ_DIR) clean
+	cd $(NEUTRINO_PLUGINS_OBJ_DIR); \
+		$(MAKE) -C $(NEUTRINO_PLUGINS_OBJ_DIR) clean
 
 neutrino-plugins-distclean:
-	rm -rf $(NP_OBJ_DIR)
+	rm -rf $(NEUTRINO_PLUGINS_OBJ_DIR)
 	rm -f $(D)/neutrino-plugin*
 	rm -f $(D)/neutrino.config.status
