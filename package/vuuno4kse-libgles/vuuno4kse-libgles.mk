6c4a48ff7e678f4c9a3d42fe559f5dd81c575e7d
max_10 <max_10@gmx.de>
2019-10-12 22:30:39 +0200

    - vuuno4kse added

---
+++ package/vuuno4kse-libgles/vuuno4kse-libgles.mk
@@ -0,0 +1,22 @@
+#
+# vuuno4kse-libgles
+#
+VUUNO4KSE_LIBGLES_DATE   = 20181204
+VUUNO4KSE_LIBGLES_REV    = r0
+VUUNO4KSE_LIBGLES_VER    = 17.1-$(VUUNO4KSE_LIBGLES_DATE).$(VUUNO4KSE_LIBGLES_REV)
+VUUNO4KSE_LIBGLES_SOURCE = libgles-vuuno4kse-$(VUUNO4KSE_LIBGLES_VER).tar.gz
+VUUNO4KSE_LIBGLES_URL    = http://archive.vuplus.com/download/build_support/vuplus
+
+$(ARCHIVE)/$(VUUNO4KSE_LIBGLES_SOURCE):
+	$(DOWNLOAD) $(VUUNO4KSE_LIBGLES_URL)/$(VUUNO4KSE_LIBGLES_SOURCE)
+
+$(D)/vuuno4kse-libgles: bootstrap $(ARCHIVE)/$(VUUNO4KSE_LIBGLES_SOURCE)
+	$(START_BUILD)
+	$(REMOVE)/libgles-vuuno4kse
+	$(UNTAR)/$(VUUNO4KSE_LIBGLES_SOURCE)
+	$(INSTALL_EXEC) $(BUILD_DIR)/libgles-vuuno4kse/lib/* $(TARGET_DIR)/usr/lib
+	ln -sf libv3ddriver.so $(TARGET_DIR)/usr/lib/libEGL.so
+	ln -sf libv3ddriver.so $(TARGET_DIR)/usr/lib/libGLESv2.so
+	cp -a $(BUILD_DIR)/libgles-vuuno4kse/include/* $(TARGET_DIR)/usr/include
+	$(REMOVE)/libgles-vuuno4kse
+	$(TOUCH)
---
+++ package/vuuno4kse-platform-util/vuuno4kse-platform-util.mk
@@ -0,0 +1,23 @@
+#
+# vuuno4kse-platform-util
+#
+VUUNO4KSE_PLATFORM_UTIL_DATE   = 20181204
+VUUNO4KSE_PLATFORM_UTIL_REV    = r0
+VUUNO4KSE_PLATFORM_UTIL_VER    = 17.1-$(VUUNO4KSE_PLATFORM_UTIL_DATE).$(VUUNO4KSE_PLATFORM_UTIL_REV)
+VUUNO4KSE_PLATFORM_UTIL_SOURCE = platform-util-vuuno4kse-$(VUUNO4KSE_PLATFORM_UTIL_VER).tar.gz
+VUUNO4KSE_PLATFORM_UTIL_URL    = http://archive.vuplus.com/download/build_support/vuplus
+
+$(ARCHIVE)/$(VUUNO4KSE_PLATFORM_UTIL_SOURCE):
+	$(DOWNLOAD) $(VUUNO4K_PLATFORM_UTIL_URL)/$(VUUNO4KSE_PLATFORM_UTIL_SOURCE)
+
+$(D)/vuuno4kse-platform-util: bootstrap $(ARCHIVE)/$(VUUNO4KSE_PLATFORM_UTIL_SOURCE)
+	$(START_BUILD)
+	$(REMOVE)/platform-util-vuuno4kse
+	$(UNTAR)/$(VUUNO4KSE_PLATFORM_UTIL_SOURCE)
+	$(INSTALL_EXEC) $(BUILD_DIR)/platform-util-vuuno4kse/* $(TARGET_DIR)/usr/bin
+	$(REMOVE)/platform-util-vuuno4kse
+	$(INSTALL_EXEC) $(PKG_FILES_DIR)/vuplus-platform-util $(TARGET_DIR)/etc/init.d/vuplus-platform-util
+	$(INSTALL_EXEC) $(PKG_FILES_DIR)/vuplus-shutdown $(TARGET_DIR)/etc/init.d/vuplus-shutdown
+	$(HELPERS_DIR)/update-rc.d -r $(TARGET_DIR) vuplus-platform-util start 65 S . stop 90 0 .
+	$(HELPERS_DIR)/update-rc.d -r $(TARGET_DIR) vuplus-shutdown start 89 0 .
+	$(TOUCH)
