cf6f57b3b7c352d8835c30648b04f8525c4712b4
max_10 <max_10@gmx.de>
2019-10-12 22:29:40 +0200

    - vuuno4k added

---
+++ package/vuuno4k-driver/vuuno4k-driver.mk
@@ -0,0 +1,21 @@
+#
+# vuuno-driver
+#
+VUUNO4K_DRIVER_DATE   = 20181204
+VUUNO4K_DRIVER_REV    = r0
+VUUNO4K_DRIVER_VER    = 3.14.28-$(VUUNO4K_DRIVER_DATE).$(VUUNO4K_DRIVER_REV)
+VUUNO4K_DRIVER_SOURCE = vuplus-dvb-proxy-vuuno4k-$(VUUNO4K_DRIVER_VER).tar.gz
+VUUNO4K_DRIVER_URL    = http://archive.vuplus.com/download/build_support/vuplus
+
+$(ARCHIVE)/$(VUUNO4K_DRIVER_SOURCE):
+	$(DOWNLOAD) $(VUUNO4K_DRIVER_URL)/$(VUUNO4K_DRIVER_SOURCE)
+
+$(D)/vuuno4k-driver: bootstrap $(ARCHIVE)/$(VUUNO4K_DRIVER_SOURCE)
+	$(START_BUILD)
+	mkdir -p $(TARGET_DIR)/lib/modules/$(KERNEL_VER)/extra
+	tar -xf $(ARCHIVE)/$(VUUNO4K_DRIVER_SOURCE) -C $(TARGET_DIR)/lib/modules/$(KERNEL_VER)/extra
+	make depmod
+	$(TOUCH)
+	make vuuno4k-platform-util
+	make vuuno4k-libgles
+	make vuuno4k-vmlinuz-initrd
