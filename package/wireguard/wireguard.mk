1db6bbd8b147b64284ae6e42006d955e9e6ecb71
max_10 <max_10@gmx.de>
2019-10-18 13:44:53 +0200

    - add wireguard

---
+++ package/wireguard/wireguard.mk
@@ -0,0 +1,32 @@
+#
+# wireguard
+#
+WIREGUARD_VER    = 0.0.20191012
+WIREGUARD_DIR    = WireGuard-$(WIREGUARD_VER)
+WIREGUARD_SOURCE = WireGuard-$(WIREGUARD_VER).tar.xz
+WIREGUARD_URL    = https://git.zx2c4.com/WireGuard/snapshot
+
+$(ARCHIVE)/$(WIREGUARD_SOURCE):
+	$(DOWNLOAD) $(WIREGUARD_URL)/$(WIREGUARD_SOURCE)
+
+WIREGUARD_PATCH = \
+	wireguard.patch
+
+WIREGUARD_MAKE_OPTS = WITH_SYSTEMDUNITS=no WITH_BASHCOMPLETION=no WITH_WGQUICK=no
+
+$(D)/wireguard: bootstrap kernel libmnl $(ARCHIVE)/$(WIREGUARD_SOURCE)
+	$(START_BUILD)
+	$(REMOVE)/$(WIREGUARD_DIR)
+	$(UNTAR)/$(WIREGUARD_SOURCE)
+	$(CHDIR)/$(WIREGUARD_DIR)/src; \
+		$(call apply_patches, $(WIREGUARD_PATCH)); \
+		$(BUILD_ENV) \
+		$(MAKE) all     $(KERNEL_MAKEVARS) KERNELDIR=$(KERNEL_MODULES_DIR)/build $(WIREGUARD_MAKE_OPTS) PREFIX=/usr; \
+		$(MAKE) install $(KERNEL_MAKEVARS) KERNELDIR=$(KERNEL_MODULES_DIR)/build $(WIREGUARD_MAKE_OPTS) INSTALL_MOD_PATH=$(TARGET_DIR) DESTDIR=$(TARGET_DIR) MANDIR=/.remove
+	mkdir -p ${TARGET_DIR}/etc/modules-load.d
+	for i in wireguard.ko; do \
+		echo $$i >> ${TARGET_DIR}/etc/modules-load.d/wireguard.conf; \
+	done
+	make depmod
+	$(REMOVE)/$(WIREGUARD_DIR)
+	$(TOUCH)
