dd2b79d6d3730d53a82e4638fb032024d23de459
max_10 <max_10@gmx.de>
2019-10-22 21:24:47 +0200

    - osmio4k: driver update

--- package/osmio4k-driver/osmio4k-driver.mk
+++ package/osmio4k-driver/osmio4k-driver.mk
@@ -1,7 +1,7 @@
 #
 # osmio4k-driver
 #
-OSMIO4K_DRIVER_DATE   = 20190917
+OSMIO4K_DRIVER_DATE   = 20191010
 OSMIO4K_DRIVER_VER    = 5.3.0-$(OSMIO4K_DRIVER_DATE)
 OSMIO4K_DRIVER_SOURCE = osmio4k-drivers-$(OSMIO4K_DRIVER_VER).zip
 OSMIO4K_DRIVER_URL    = http://source.mynonpublic.com/edision
