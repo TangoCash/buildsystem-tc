c188134bb977a846bdcbc91c61bb26eacdb69a1a
max_10 <max_10@gmx.de>
2020-01-11 12:07:00 +0100

    - sysvinit: bump version to 2.96

--- package/sysvinit/sysvinit.mk
+++ package/sysvinit/sysvinit.mk
@@ -1,9 +1,9 @@
 #
 # sysvinit
 #
-SYSVINIT_VER    = 2.88dsf
+SYSVINIT_VER    = 2.96
 SYSVINIT_DIR    = sysvinit-$(SYSVINIT_VER)
-SYSVINIT_SOURCE = sysvinit-$(SYSVINIT_VER).tar.bz2
+SYSVINIT_SOURCE = sysvinit-$(SYSVINIT_VER).tar.xz
 SYSVINIT_URL    = http://download.savannah.nongnu.org/releases/sysvinit
 
 $(ARCHIVE)/$(SYSVINIT_SOURCE):
@@ -11,10 +11,9 @@ $(ARCHIVE)/$(SYSVINIT_SOURCE):
 
 SYSVINIT_PATCH  = \
 	crypt-lib.patch \
+	change-INIT_FIFO.patch \
 	pidof-add-m-option.patch \
-	realpath.patch \
-	0001-include-sys-sysmacros.h-for-major-minor-defines-in-g.patch \
-	0001-This-fixes-an-issue-that-clang-reports-about-mutlipl.patch
+	realpath.patch
 
 $(D)/sysvinit: bootstrap $(ARCHIVE)/$(SYSVINIT_SOURCE)
 	$(START_BUILD)
@@ -23,14 +22,14 @@ $(D)/sysvinit: bootstrap $(ARCHIVE)/$(SYSVINIT_SOURCE)
 	$(CHDIR)/$(SYSVINIT_DIR); \
 		$(call apply_patches, $(SYSVINIT_PATCH)); \
 		$(BUILD_ENV) \
-		$(MAKE) -C src SULOGINLIBS=-lcrypt; \
+		$(MAKE) SULOGINLIBS=-lcrypt; \
 		$(MAKE) install ROOT=$(TARGET_DIR) MANDIR=/.remove
 	$(INSTALL_DATA) $(PKG_FILES_DIR)/inittab $(TARGET_DIR)/etc/inittab
 	$(INSTALL_EXEC) $(PKG_FILES_DIR)/autologin $(TARGET_DIR)/bin/autologin
 	$(INSTALL_EXEC) $(PKG_FILES_DIR)/rc $(TARGET_DIR)/etc/init.d
 	$(INSTALL_EXEC) $(PKG_FILES_DIR)/rcS $(TARGET_DIR)/etc/init.d
 	$(INSTALL_DATA) $(PKG_FILES_DIR)/rcS-default $(TARGET_DIR)/etc/default/rcS
-	$(INSTALL_EXEC) $(PKG_FILES_DIR)/service $(TARGET_DIR)/sbin/
+	$(INSTALL_EXEC) $(PKG_FILES_DIR)/service $(TARGET_DIR)/sbin/service
 #	$(INSTALL_EXEC) $(PKG_FILES_DIR)/bootlogd.init $(TARGET_DIR)/etc/init.d/bootlogd
 #	$(INSTALL_DATA) $(PKG_FILES_DIR)/volatiles.01_bootlogd $(TARGET_DIR)/etc/default/volatiles/01_bootlogd
 #	ln -sf bootlogd $(TARGET_DIR)/etc/init.d/stop-bootlogd
