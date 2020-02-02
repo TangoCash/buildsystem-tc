ea3b9f74bfa9e87ffacbb576b28f0db889e66059
max_10 <max_10@gmx.de>
2020-02-02 11:27:19 +0100

    - udpxy: fix build > gcc7

--- package/udpxy/udpxy.mk
+++ package/udpxy/udpxy.mk
@@ -2,12 +2,13 @@
 # udpxy
 #
 UDPXY_VER    = git
-UDPXY_DIR    = udpxy.$(UDPXY_VER)
+UDPXY_DIR    = udpxy.git
 UDPXY_SOURCE = $(UDPXY_DIR)
 UDPXY_URL    = https://github.com/pcherenkov/$(UDPXY_SOURCE)
 
 UDPXY_PATCH  = \
-	udpxy.patch
+	udpxy.patch \
+	fix-build-with-gcc8.patch
 
 $(D)/udpxy: bootstrap
 	$(START_BUILD)
@@ -17,7 +18,7 @@ $(D)/udpxy: bootstrap
 	$(CHDIR)/$(UDPXY_DIR)/chipmunk; \
 		$(call apply_patches, $(UDPXY_PATCH)); \
 		$(BUILD_ENV) \
-		$(MAKE) CC=$(TARGET_CC) CCKIND=gcc; \
+		$(MAKE) CC=$(TARGET_CC); \
 		$(MAKE) install INSTALLROOT=$(TARGET_DIR)/usr MANPAGE_DIR=$(TARGET_DIR)/.remove
 	$(REMOVE)/$(UDPXY_DIR)
 	$(TOUCH)
