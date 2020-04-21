c83349981d624bdd6947043ad5fad9f4ab6d7bd7
max_10 <max_10@gmx.de>
2020-04-21 20:54:09 +0200

    - ofgwrite: update build

--- package/ofgwrite/ofgwrite.mk
+++ package/ofgwrite/ofgwrite.mk
@@ -6,16 +6,12 @@ OFGWRITE_DIR    = ofgwrite-nmp.$(OFGWRITE_VER)
 OFGWRITE_SOURCE = $(OFGWRITE_DIR)
 OFGWRITE_URL    = $(MAX-GIT-GITHUB)/$(OFGWRITE_SOURCE)
 
-OFGWRITE_PATCH  = \
-	fix_glibc_major.patch
-
 $(D)/ofgwrite: bootstrap
 	$(START_BUILD)
 	$(REMOVE)/$(OFGWRITE_DIR)
 	$(GET-GIT-SOURCE) $(OFGWRITE_URL) $(ARCHIVE)/$(OFGWRITE_SOURCE)
 	$(CPDIR)/$(OFGWRITE_DIR)
 	$(CHDIR)/$(OFGWRITE_DIR); \
-		$(call apply_patches, $(OFGWRITE_PATCH)); \
 		$(BUILD_ENV) \
 		$(MAKE); \
 	$(INSTALL_EXEC) $(BUILD_DIR)/$(OFGWRITE_DIR)/ofgwrite_bin $(TARGET_DIR)/usr/bin
