37651e69a7c339975a47bf0ac4529c7813a5322c
max_10 <max_10@gmx.de>
2020-05-07 01:51:26 +0200

    - helpers: update caseconvert-helper

--- make/helpers.mk
+++ make/helpers.mk
@@ -91,8 +91,8 @@ endef
 # Case conversion macros.
 #
 
-[LOWER] := a b c d e f g h i j k l m n o p q r s t u v w x y z
-[UPPER] := A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+[LOWER] := a b c d e f g h i j k l m n o p q r s t u v w x y z - .
+[UPPER] := A B C D E F G H I J K L M N O P Q R S T U V W X Y Z _ _
 
 define caseconvert-helper
 $(1) = $$(strip \
