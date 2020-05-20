d0a6489d2d28f094febf1e77768b7c2b8f857662
max_10 <max_10@gmx.de>
2020-05-20 15:12:15 +0200

    - gptfdisk: bump version to 1.0.5

--- package/gptfdisk/gptfdisk.mk
+++ package/gptfdisk/gptfdisk.mk
@@ -1,7 +1,7 @@
 #
 # gptfdisk
 #
-GPTFDISK_VER    = 1.0.4
+GPTFDISK_VER    = 1.0.5
 GPTFDISK_DIR    = gptfdisk-$(GPTFDISK_VER)
 GPTFDISK_SOURCE = gptfdisk-$(GPTFDISK_VER).tar.gz
 GPTFDISK_URL    = https://sourceforge.net/projects/gptfdisk/files/gptfdisk/$(GPTFDISK_VER)
@@ -9,15 +9,11 @@ GPTFDISK_URL    = https://sourceforge.net/projects/gptfdisk/files/gptfdisk/$(GPT
 $(ARCHIVE)/$(GPTFDISK_SOURCE):
 	$(DOWNLOAD) $(GPTFDISK_URL)/$(GPTFDISK_SOURCE)
 
-GPTFDISK_PATCH  = \
-	ldlibs.patch
-
 $(D)/gptfdisk: bootstrap e2fsprogs ncurses popt $(ARCHIVE)/$(GPTFDISK_SOURCE)
 	$(START_BUILD)
 	$(REMOVE)/$(GPTFDISK_DIR)
 	$(UNTAR)/$(GPTFDISK_SOURCE)
 	$(CHDIR)/$(GPTFDISK_DIR); \
-		$(call apply_patches, $(GPTFDISK_PATCH)); \
 		$(BUILD_ENV) \
 		$(MAKE) sgdisk; \
 		$(INSTALL_EXEC) sgdisk $(TARGET_DIR)/usr/sbin/sgdisk
