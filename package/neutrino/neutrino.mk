a808482ff58ddb9a98c2a239389630813ff2d4e3
max_10 <max_10@gmx.de>
2020-05-22 20:53:40 +0200

    - add host-ninja

--- package/neutrino/neutrino.mk
+++ package/neutrino/neutrino.mk
@@ -169,7 +169,7 @@ NEUTRINO_BRANCH   ?= master
 LIBSTB_HAL_BRANCH ?= master
 NEUTRINO_PATCH     =
 LIBSTB_HAL_PATCH   =
-else ifeq  ($(FLAVOUR), neutrino-ni)
+else ifeq ($(FLAVOUR), neutrino-ni)
 GIT_URL           ?= https://github.com/neutrino-images
 NEUTRINO           = ni-neutrino
 LIBSTB_HAL         = ni-libstb-hal
@@ -177,7 +177,7 @@ NEUTRINO_BRANCH   ?= master
 LIBSTB_HAL_BRANCH ?= master
 NEUTRINO_PATCH     =
 LIBSTB_HAL_PATCH   =
-else ifeq  ($(FLAVOUR), neutrino-tangos)
+else ifeq ($(FLAVOUR), neutrino-tangos)
 GIT_URL           ?= https://github.com/TangoCash
 NEUTRINO           = neutrino-tangos
 LIBSTB_HAL         = libstb-hal-tangos
@@ -185,7 +185,7 @@ NEUTRINO_BRANCH   ?= master
 LIBSTB_HAL_BRANCH ?= master
 NEUTRINO_PATCH     =
 LIBSTB_HAL_PATCH   =
-else ifeq  ($(FLAVOUR), neutrino-ddt)
+else ifeq ($(FLAVOUR), neutrino-ddt)
 GIT_URL           ?= https://github.com/Duckbox-Developers
 NEUTRINO           = neutrino-ddt
 LIBSTB_HAL         = libstb-hal-ddt
