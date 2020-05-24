b99e396c87a22ff2ef832200a10cdaf79d7839dd
max_10 <max_10@gmx.de>
2020-05-24 10:47:23 +0200

    - bash: update patchset

--- package/bash/bash.mk
+++ package/bash/bash.mk
@@ -9,25 +9,12 @@ BASH_URL    = http://ftp.gnu.org/gnu/bash
 $(ARCHIVE)/$(BASH_SOURCE):
 	$(DOWNLOAD) $(BASH_URL)/$(BASH_SOURCE)
 
-BASH_PATCH  = \
-	bash50-001.patch \
-	bash50-002.patch \
-	bash50-003.patch \
-	bash50-004.patch \
-	bash50-005.patch \
-	bash50-006.patch \
-	bash50-007.patch \
-	bash50-008.patch \
-	bash50-009.patch \
-	bash50-010.patch \
-	bash50-011.patch
-
 $(D)/bash: bootstrap ncurses $(ARCHIVE)/$(BASH_SOURCE)
 	$(START_BUILD)
 	$(REMOVE)/$(BASH_DIR)
 	$(UNTAR)/$(BASH_SOURCE)
 	$(CHDIR)/$(BASH_DIR); \
-		$(call apply_patches, $(BASH_PATCH), 0); \
+		$(call apply_patches, $(PKG_PATCHES_DIR), 0); \
 		$(CONFIGURE); \
 		$(MAKE); \
 		$(INSTALL_EXEC) bash $(TARGET_DIR)/bin
